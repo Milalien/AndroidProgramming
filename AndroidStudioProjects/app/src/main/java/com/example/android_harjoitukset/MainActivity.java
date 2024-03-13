d7c62243734849fd57949d7e584e0d7f3f643b9a
Milla Kärkkäinen <karkkimilla@gmail.com>
2024-03-13 15:27:04 +0200

    130324

--- AndroidStudioProjects/app/src/main/java/com/example/android_harjoitukset/MainActivity.java
+++ AndroidStudioProjects/app/src/main/java/com/example/android_harjoitukset/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.android_harjoitukset;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,8 +13,12 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button startButton;
+//private Button gameButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -20,5 +27,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        startButton = (Button) findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                handleOnClickEvents(v);
+
+            }
+        });
+    }
+
+    private void handleOnClickEvents(View v) {
+        switch (v.getId()) {
+            case R.id.startButton:
+                Log.d("MainActivity", "User clicked the Start-button");
+                break;
+            default:
+        }
     }
 }
